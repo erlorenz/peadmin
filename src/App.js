51b947772a9c1d86a74d11be8fe9cc47dda6d72b
erlorenz <38394390+erlorenz@users.noreply.github.com>
2018-10-12 15:04:55 -0700

    added lint prettier axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
 
-
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        BACK TO NORMAL
-      </div>
-    );
+    return <div className="App">BACK TO NORMAL</div>;
   }
 }
 
