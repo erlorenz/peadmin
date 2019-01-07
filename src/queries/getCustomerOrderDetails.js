17ebad051a325d470751bcd7ea1fa1d83feefea2
Erik Lorenz <erikslorenz@gmail.com>
2019-01-07 02:36:45 -0800

    added order queries

---
+++ src/queries/getCustomerOrderDetails.js
@@ -0,0 +1,49 @@
+import gql from 'graphql-tag';
+
+export const GET_CUSTOMER_ORDER = gql`
+  query GetCustomerOrderDetails($id: ID!) {
+    getCustomerOrderDetails(customer_order_id: $id) {
+      status
+      total_price
+      name
+      id
+      created_at
+      pickup_date
+      return_date
+      email
+      hotel
+      phone
+      room
+      starch
+      special_instructions
+      stripe_charge
+      stripe_customer
+      adminComments {
+        created_at
+        comment_body
+        name
+      }
+      picked_up
+      checked_in
+      out_for_delivery
+      completed
+      refunds {
+        amount
+        stripe_refund
+      }
+      additionalCharges {
+        amount
+        stripe_charge
+      }
+      text_sent
+      receipt_sent
+      customerOrderItems {
+        description
+        id
+        slug
+        quantity
+        price
+      }
+    }
+  }
+`;
